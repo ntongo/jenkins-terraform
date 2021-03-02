8f9a001b3798a4f0f09ea5eb469ad59ce875b2fe
ntongo <75582445+ntongo@users.noreply.github.com>
2021-03-01 22:38:40 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "jjtech-ci-cd-terraform"
+    bucket = "cicd-terraform09"
     key    = "state/jjtech.tfstate"
-    region = "us-west-1"
+    region = "us-east-1"
     profile = "default"
   }
 }
